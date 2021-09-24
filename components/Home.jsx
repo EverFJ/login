eb501c01327bf73d4648b30eca74cd0afef6d0c2
Francois Janson <jansonfrancois@gmail.com>
2021-09-24 13:39:26 +0200

    routing logic

--- components/Home.jsx
+++ components/Home.jsx
@@ -1,11 +1,21 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { Button, StyleSheet, Text, View } from "react-native";
 import { StatusBar } from "expo-status-bar";
 
-export default function Home() {
+export default function Home(props) {
+  //   console.log(`Home props.location`, props.location);
   return (
     <View style={styles.container}>
       <Text>Home</Text>
+      <Text>Welcome {props.location.state.username}</Text>
+      <Button
+        title="Logout"
+        onPress={() => {
+          props.history.push("/", {
+            username: props.location.state.username,
+          });
+        }}
+      />
       <StatusBar style="auto" />
     </View>
   );
--- components/Login.jsx
+++ components/Login.jsx
@@ -1,21 +1,44 @@
 import React, { useState } from "react";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { Button, StyleSheet, Text, View } from "react-native";
 import { TextInput } from "react-native-gesture-handler";
 
-export default function Login() {
-  const [username, setUsername] = useState("");
+export default function Login(props) {
+  const [username, setUsername] = useState(
+    props.location.state ? props.location.state.username : ""
+  );
   const [password, setPassword] = useState("");
 
+  console.log(`Login props.location`, props.location);
   return (
     <View style={styles.container}>
-      <Text>Please Login</Text>
-      <TextInput
-        value={username}
-        onChangeText={handleUsername}
-        placeholder="Enter your username"
-      />
-      <TextInput value={password} onChangeText={handlePassword} />
+      <Text style={styles.title}>Please Login</Text>
+      <View style={styles.containerLogin}>
+        <Text>Username : </Text>
+        <TextInput
+          style={styles.input}
+          value={username}
+          onChangeText={setUsername}
+          placeholder="Enter your username"
+          autoFocus
+        />
+        <Text>Password : </Text>
+        <TextInput
+          style={styles.input}
+          value={password}
+          onChangeText={setPassword}
+          placeholder="Enter your password"
+        />
+        {username.length > 5 && password.length > 5}
+        <Button
+          title="Submit"
+          disabled={username.length < 5 || password.length < 5}
+          onPress={() => {
+            props.history.push("/home", { username: username });
+          }}
+        />
+      </View>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -27,5 +50,20 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+  },
+  containerLogin: {
+    backgroundColor: "#eee",
+    padding: 16,
+    borderRadius: 10,
+  },
+  title: {
+    fontSize: 36,
+    marginVertical: 10,
+  },
+  input: {
+    padding: 8,
+    borderColor: "#000",
+    borderWidth: 1,
+    margin: 8,
   },
 });
